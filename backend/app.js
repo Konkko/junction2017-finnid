291aaee55115894a1c5ba3b207d1b50828f527a4
Mikael Eriksson <migluuu@gmail.com>
2017-11-24 21:53:03 +0000

    Backend API thingy

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,29 @@
 // Backend
+
+var express             = require("express");
+var bodyParser          = require("body-parser");
+var apiRoutes           = require("./routes/api_routes");
+
+var app                 = express();
+var IP                  = process.env.IP,
+    PORT                = process.env.PORT || 8081;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+// REST routes
+ app.use("/api", apiRoutes);
+
+app.get("/", (req, res) => {
+    res.send("API");
+});
+
+// 404
+app.get("*", function(req, res, next) {
+    res.status(404).send("There is no such thing");
+});
+
+
+// SERVER LISTENER
+app.listen(PORT, IP, function() {
+    console.log('Server is listening');
+});
--- backend/routes/api_routes.js
+++ backend/routes/api_routes.js
@@ -2,3 +2,15 @@
 // API ROUTES
 // =============================================================================
 
+var express             = require("express");
+var router              = express.Router();
+
+var response = {res: "RESPOOOONSE"};
+
+router.get("/Epc/GetAll", function(req, res){
+    res.send(JSON.stringify(response))
+});
+
+module.exports = router;
+
+// /api/Epc/GetById/{id}
