5b99d99e6a2ff363aa50c658bf726cd81b693f56
Mikael Eriksson <migluuu@gmail.com>
2017-11-25 15:29:44 +0000

    /Item/GetAll end point added

--- backend/routes/api_routes.js
+++ backend/routes/api_routes.js
@@ -16,27 +16,30 @@ router.get("/Epc/GetAll", function(req, res){
 
 //One data point in history
 router.get("/Epc/GetById/:id", function(req, res){
-    console.log(global.EPC);
-    console.log(req.params.id);
     var epc = _.filter(global.EPC, function(o) {return o.id == req.params.id});
-    console.log(epc);
     res.send(JSON.stringify(epc[0]));
 });
 
 //All history data of one specified epc tag
 router.get("/Epc/GetByEpc/:epc", function(req, res) {
     var epc = _.filter(global.EPC, {epcCode: req.params.epc});
-    console.log(epc);
 
    res.send(JSON.stringify({epc}));
 });
 
-//All epc tags
+//All epc tags with latest location
 router.get("/Item/GetAll", function(req, res) {
     var uniqueEpc = _.uniqBy(global.EPC, 'epcCode');
-    console.log(uniqueEpc);
-    
-    res.send(JSON.stringify({res: "All Items"}));
+
+    var response = uniqueEpc.map(function(uniqObj){
+        var asd = _.findLast(global.EPC, function(o){
+            return o.epcCode == uniqObj.epcCode;
+        });
+        
+        return asd;
+        
+    });
+    res.send(JSON.stringify(response));
 });
 
 router.get("/Item/GetById/:id", function(req, res) {
