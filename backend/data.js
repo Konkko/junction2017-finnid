99b68ecc2e0f5b8502a32a47b6f86ef342e75aa0
Mikael Eriksson <migluuu@gmail.com>
2017-11-25 14:28:36 +0000

    History saving added, end points added

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,5 @@
 var axios           = require("axios");
+var _               = require("lodash");
 
 
 
@@ -11,10 +12,17 @@ module.exports = {
         setInterval(function(){
             axios.get("http://finnidhackjunctionrfidwebapi.azurewebsites.net/api/Epc/GetAll")
             .then(function(res) {
-                global.EPC = res.data;
-                console.log(global.EPC);
+                if (!global.EPC){
+                    global.EPC = _.sortBy(res.data, ['id']);
+                } else {
+                    var newValues = _.filter(res.data, function(o){ 
+                            return parseInt(o.id) > parseInt(global.EPC[global.EPC.length-1].id)})
+                    newValues = _.sortBy(newValues, ['id']);
+                    global.EPC = global.EPC.concat(newValues);
+                    
+                }
             })}
-            ,1000)
+            ,2000)
     }
 
 }
--- backend/routes/api_routes.js
+++ backend/routes/api_routes.js
@@ -6,20 +6,29 @@ var express             = require("express");
 var router              = express.Router();
 //var faker               = require("faker");
 var axios               = require("axios");
+var bodyParser          = require("body-parser");
 var _                   = require("lodash");
-
-var response = {res: "RESPOOOONSE"};
 
 router.get("/Epc/GetAll", function(req, res){
     res.send(global.EPC);
 });
 
 router.get("/Epc/GetById/:id", function(req, res){
-    res.send(JSON.stringify({res: "ID"}));
+    console.log(global.EPC);
+    console.log(req.params.id);
+    var epc = _.filter(global.EPC, function(o) {return o.id == req.params.id});
+    console.log(epc);
+    res.send(JSON.stringify(epc[0]));
+
+    
+    
 });
 
 router.get("/Epc/GetByEpc/:epc", function(req, res) {
-   res.send(JSON.stringify({res: "EPC"}));
+    var epc = _.filter(global.EPC, {epcCode: req.params.epc});
+    console.log(epc);
+
+   res.send(JSON.stringify({epc}));
 });
 
 router.get("/Item/GetAll", function(req, res) {
