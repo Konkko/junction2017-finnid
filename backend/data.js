a81332fc99e235dc529a402bcf3b97b284899f2a
Jari Tuikka <jari.tuikka@gmail.com>
2017-11-25 03:14:21 +0200

    Fix response

--- backend/data.js
+++ backend/data.js
@@ -5,7 +5,7 @@ module.exports = {
         setInterval(function(){
             axios.get("http://finnidhackjunctionrfidwebapi.azurewebsites.net/api/Epc/GetAll")
             .then(function(res) {
-                global.EPC = res;
+                global.EPC = res.data;
                 console.log(global.EPC);
             })}
             ,1000)
