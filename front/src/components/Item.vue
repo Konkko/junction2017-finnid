cbb38a240d28c848b22f0b9946028e6baa462ba8
Niko Autio <niko.autio@fenten.fi>
2017-11-25 18:19:42 +0200

    Items have id title

--- front/src/components/Item.vue
+++ front/src/components/Item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="item" :style="style">
+  <div class="item" :style="style" :title="item.epc">
     <!--<img src="../assets/item.png" />-->
   </div>
 </template>
@@ -7,10 +7,10 @@
 <script>
   export default {
     name: "Item",
-    props: ['location'],
+    props: ['item'],
     computed: {
       style() {
-        return "position: absolute; left: " + this.location.x + "px; top: " + this.location.y + "px; background-color: red; width: 15px; height: 15px;"
+        return "position: absolute; left: " + this.item.lastLocation.x + "px; top: " + this.item.lastLocation.y + "px; background-color: red; width: 15px; height: 15px;"
       }
     },
     data() {
